d32088a51ea931fb9ea44f2a50d203f24ba964e0
Tushar <tusharimran470@gmail.com>
2024-07-02 12:06:26 +0600

    banner section is adde4d

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,48 +1,97 @@
-import React from 'react';
+import Image from 'next/image'
+import slide1 from '@/assest/slide-01.webp'
+import slide2 from '@/assest/slide-02.webp'
+
+
+
 
 const Banner = () => {
+
+
+    const bannerInfo=[
+        {
+            id:1,
+            image:'https://i.ibb.co/PY2FyTQ/banner-01.webp',
+            description:'On selected Womens footwear',
+            discount:'20% OFF'
+        },
+        {
+            id:2,
+            image:"https://i.ibb.co/x32Z4t0/banner-02.webp",
+            description:'On selected mens fashion',
+            discount:'25% OFF'
+        },
+        {
+            id:3,
+            image:"https://i.ibb.co/6NRrnrg/banner-03.webp",
+            description:'On selected kids fashion',
+            discount:'30% OFF'
+        },
+    ]
+
     return (
         <div>
+            {/* carosel */}
             <div className="carousel w-full">
-  <div id="slide1" className="carousel-item relative w-full">
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg"
-      className="w-full" />
-    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-      <a href="#slide4" className="btn btn-circle">❮</a>
-      <a href="#slide2" className="btn btn-circle">❯</a>
-    </div>
-  </div>
-  <div id="slide2" className="carousel-item relative w-full">
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg"
-      className="w-full" />
-    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-      <a href="#slide1" className="btn btn-circle">❮</a>
-      <a href="#slide3" className="btn btn-circle">❯</a>
-    </div>
-  </div>
-  <div id="slide3" className="carousel-item relative w-full">
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg"
-      className="w-full" />
-    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-      <a href="#slide2" className="btn btn-circle">❮</a>
-      <a href="#slide4" className="btn btn-circle">❯</a>
-    </div>
-  </div>
-  <div id="slide4" className="carousel-item relative w-full">
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg"
-      className="w-full" />
-    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-      <a href="#slide3" className="btn btn-circle">❮</a>
-      <a href="#slide1" className="btn btn-circle">❯</a>
-    </div>
-  </div>
-</div>
-        </div>
-    );
-};
 
-export default Banner;
+                <div id="slide1" className="carousel-item relative w-full">
+                     
+              <div className='relative'>
+                   <Image src={slide1} alt="banner1" height={1200} width={1200} />
+                   <div className='absolute top-72 left-24'> 
+                    <h3 className="text-white font-semibold">Up to 70% off</h3>
+                    <h2 className='text-xl text-white py-3 font-bold'>WOMENS CLOTHING</h2>
+                     <button className='p-3 bg-white hover:bg-transparent font-bold hover:text-white'>Shop Now</button>
+                   </div>
+
+              </div>
+                     
+
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide2" className="btn btn-circle">❮</a>
+                        <a href="#slide2" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+
+                <div id="slide2" className="carousel-item relative w-full">
+                   
+                <div className='relative'>
+                   <Image src={slide2} alt="banner2" height={1200} width={1200} />
+                   <div className='absolute top-72 right-24'> 
+                    <h3 className="text-white font-semibold">Up to 70% off</h3>
+                    <h2 className='text-xl text-white py-3 font-bold'>WOMENS CLOTHING</h2>
+                     <button className='p-3 bg-white hover:bg-transparent font-bold hover:text-white'>Shop Now</button>
+                   </div>
+
+              </div>
+
+
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide1" className="btn btn-circle">❮</a>
+                        <a href="#slide1" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+
+            </div>
+
+             <div className='mt-10 flex gap-6'>
+                    {
+                        bannerInfo.map(card=><div key={card?.id}>
+                             <div className='relative'>
+                                 <Image src={card?.image} alt=".." height={500} width={500} />
+                                 <h1 className="text-lg w-full p-2 bg-gray-600 text-white text-center">{card?.description}</h1>
+
+                                 <span className='py-7 px-2  absolute  top-60 right-1 rounded-full text-white  bg-[#FF8A65]'>
+                                    {card?.discount}
+                                 </span>
+                             </div>
+                        </div>)
+                    }
+             </div>
+
+
+        </div>
+    )
+}
+
+export default Banner
--- src/app/page.js
+++ src/app/page.js
@@ -5,11 +5,23 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div className='w-[95%] mx-auto'>
-             <Nav/>
-              <Banner/>
-              <Services/>
-             Home
+        <div className='w-[100%] mx-auto '>
+            <Nav />
+            <div className='flex gap-6 w-[80%] mx-auto mt-10'>
+                <div className='w-[20%]'>
+                     {/* leftSideContents */}
+                      left side contents
+                </div>
+                <div className='w-[80%]'>
+
+                    <Banner />
+                   
+                </div>
+
+            </div>
+
+
+
         </div>
     );
 };
